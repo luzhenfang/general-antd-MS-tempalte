2983a8dd3c4f39c0ef46a95c019c0f70790e905a
luzhenfang <1318659507@qq.com>
2023-04-03 16:52:27 +0800

    添加组件懒加载loading

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,22 @@
+import { Spin } from "antd";
+import React from "react";
 import { lazy } from "react";
 import { Navigate, RouteObject } from "react-router-dom";
 
 
 const Home = lazy(() => import("@/views/Home"));
 const About = lazy(() => import("@/views/About"));
+
+
+// 懒加载组件 loading
+const LazyLoading: React.FC = () => {
+    return (
+        <div className="loading-box">
+            <Spin />
+        </div>
+    )
+}
+
 
 const routes: Array<RouteObject> = [
     {
@@ -12,7 +25,7 @@ const routes: Array<RouteObject> = [
     },
     {
         path: "/home",
-        element: <Home />
+        element: <React.Suspense fallback={<LazyLoading />}><Home /> </React.Suspense>
     },
     {
         path: "/about",
