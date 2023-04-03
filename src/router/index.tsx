282e55e8b12aab48b9f28beda93c946d174af754
luzhenfang <1318659507@qq.com>
2023-04-03 16:33:40 +0800

    实现首页跳转home

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,13 @@
 import App from "@/App"
 import About from "@/views/About"
 import Home from "@/views/Home"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 const baseRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
+                <Route path="/" element={<Navigate to={"/home"} />}></Route>
                 <Route path="/home" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
             </Route>
