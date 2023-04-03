800e8cf7050a4680e89ac4bcee5c4c9b63c68ebb
luzhenfang <1318659507@qq.com>
2023-04-03 15:43:29 +0800

    引入路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "reset-css";
-import "./assets/styles/global.scss";
+import "@/assets/styles/global.scss";
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
