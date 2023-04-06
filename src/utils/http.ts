dc7373939a828443fc98c40b871d1fda6bfc2936
luzhenfang <1318659507@qq.com>
2023-04-07 00:48:31 +0800

    token 请求头注入

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "./token";
 
 const http = axios.create({
     baseURL: "http://localhost:8080",
@@ -6,6 +7,10 @@ const http = axios.create({
 })
 
 http.interceptors.request.use((config) => {
+    const token = getToken();
+    // token 无效
+    if (!token) return config;
+    config.headers.Authorization = `Bearer ${token}`
     return config;
 }, (error) => {
     return Promise.reject(error);
@@ -14,6 +19,9 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use((response) => {
     return response;
 }, (error) => {
+    if (error.response.status === 401) {
+        console.log("need login.")
+    }
     return Promise.reject(error);
 })
 
