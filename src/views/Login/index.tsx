b37b002d9e992f38734f9a94b23d9cfafd637c87
luzhenfang <1318659507@qq.com>
2023-04-06 23:55:51 +0800

    登录布局实现

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Input, Space } from "antd";
 import styles from "./login.module.scss";
 const view: React.FC = () => {
     return (
@@ -6,6 +7,13 @@ const view: React.FC = () => {
                 <div className={styles.title}>
                     <h1>通用后台管理模板</h1>
                     <p>react + typescript + vite</p>
+                    <div className="form">
+                        <Space direction="vertical" size={"middle"} style={{ display: "flex" }}>
+                            <Input placeholder="用户名" className={styles.ant_input}></Input>
+                            <Input.Password placeholder="用户密码"></Input.Password>
+                            <Button type="primary" block>登录</Button>
+                        </Space>
+                    </div>
                 </div>
             </div>
 
