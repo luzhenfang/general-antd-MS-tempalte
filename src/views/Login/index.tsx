18c0d957f859fc6c01a4be2066545143ba73bc6b
luzhenfang <1318659507@qq.com>
2023-04-07 00:13:41 +0800

    update login & add axios

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,21 +1,19 @@
-import { Button, Input, Space } from "antd";
+import { Button, Card, Input, Space } from "antd";
 import styles from "./login.module.scss";
 const view: React.FC = () => {
     return (
         <div className={styles.login}>
-            <div className={styles.loginBox}>
-                <div className={styles.title}>
-                    <h1>通用后台管理模板</h1>
-                    <p>react + typescript + vite</p>
-                    <div className="form">
-                        <Space direction="vertical" size={"middle"} style={{ display: "flex" }}>
-                            <Input placeholder="用户名" className={styles.ant_input}></Input>
-                            <Input.Password placeholder="用户密码"></Input.Password>
-                            <Button type="primary" block>登录</Button>
-                        </Space>
-                    </div>
+            <Card className={styles.loginBox}>
+                <h1>通用后台管理模板</h1>
+                <p>react + typescript + vite</p>
+                <div className="form">
+                    <Space direction="vertical" size={"middle"} style={{ display: "flex" }}>
+                        <Input placeholder="用户名" className={styles.ant_input}></Input>
+                        <Input.Password placeholder="用户密码"></Input.Password>
+                        <Button type="primary" block>登录</Button>
+                    </Space>
                 </div>
-            </div>
+            </Card>
 
         </div>
     )
